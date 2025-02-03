0dd8185cc58fd108c9eb0e719041ec7eec000884
Gideon Ritter <gideon.ritter@enymo.com>
2025-02-03 17:35:26 +0100

    Fix date parse regex

--- src/js/util.ts
+++ src/js/util.ts
@@ -111,7 +111,7 @@ export function randomString(length: number) {
 export function dateTransformer(input: any) {
     return Object.fromEntries(Object.entries(input).map(([key, value]) => [
         key,
-        typeof value === "string" && /\d{4}-\d{2}-\d{2}(?:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:\+|-)\d{2}:\d{2})?)?/.test(value) 
+        typeof value === "string" && /^\d{4}-\d{2}-\d{2}T(?:\d{2}:\d{2}(?::\d{2}(?:\.\d+)?)?(?:Z|(?:\+|-)\d{2}:\d{2})?)?$/.test(value) 
             ? new Date(value) 
             : value
     ]));
