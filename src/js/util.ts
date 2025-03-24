42e5e3bb39d31e4bad97a31b6e46fa325ce7ee8f
Gideon Ritter <gideon.ritter@enymo.com>
2025-03-24 11:33:02 +0100

    Minor fix

--- src/js/util.ts
+++ src/js/util.ts
@@ -15,10 +15,7 @@ function isAtomic(input: any, reactNative: boolean) {
 }
 
 function isFile(input: any, reactNative: boolean) {
-    return (
-        input instanceof File
-        || (reactNative && "uri" in input && "name" in input && "type" in input)
-    )
+    return reactNative ? "uri" in input && "name" in input && "type" in input : input instanceof File;
 }
 
 export function objectNeedsFormDataConversion(input: any, reactNative: boolean): boolean {
